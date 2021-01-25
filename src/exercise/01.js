2ae4ba1e1960058d7ad3897a09dde2d9a8181bc5
Kata Szasz <Kata@invevo.com>
2021-01-25 06:50:41 +0000

    solve react-suspense 01 - extra3

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,32 +2,13 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-import {PokemonDataView, fetchPokemon, PokemonErrorBoundary} from '../pokemon'
+import {PokemonDataView, 
+        fetchPokemon, 
+        PokemonErrorBoundary,
+        PokemonInfoFallback} from '../pokemon'
+import {createResource} from '../utils'
 
 let pokemonResource = createResource(fetchPokemon('pikachu'))
-
-function createResource(promise) {
-  let status = 'pending'
-  let result = promise.then(
-    resolved => {
-      status = 'success'
-      result = resolved
-    },
-    rejected => {
-      status = 'error'
-      result = rejected
-    },
-  )
-
-  return {
-    read() {
-      if (status === 'pending') throw result
-      if (status === 'error') throw result
-      if (status === 'success') return result
-      throw new Error('This should be impossible')
-    },
-  }
-}
 
 function PokemonInfo() {
   const pokemon = pokemonResource.read()
@@ -47,7 +28,7 @@ function App() {
     <div className="pokemon-info-app">
       <div className="pokemon-info">
         <PokemonErrorBoundary>
-          <React.Suspense fallback={<div>Loading Pokemon...</div>}>
+          <React.Suspense fallback={<PokemonInfoFallback name="Pikachu" />}>
             <PokemonInfo />
           </React.Suspense>
         </PokemonErrorBoundary>
